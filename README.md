2d1163c911a66cdc5c771f5b541cd71ed4eaf28a
brysonjones <bkjones97@gmail.com>
2022-03-26 21:46:47 -0700

    first commit -- CMakeLists is up and running, and autodiff repo is included

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dynamics_sim
+
+This repo is focused on developing and experimenting with new simulation techniques, initially focusing on Contact-Implicit Trajectory Optimization using Variational Integrators
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <autodiff/forward/dual.hpp>
+
+using namespace autodiff;
+
+dual f(dual x)
+{
+    return x*x;
+}
+
+
+int main() {
+    dual x = 3;                                   // the input variable x
+    dual u = f(x);                                // the output variable u
+
+    double dudx = derivative(f, wrt(x), at(x));   // evaluate the derivative du/dx
+
+    std::cout << "u = " << u << std::endl;        // print the evaluated output u
+    std::cout << "du/dx = " << dudx << std::endl; // print the evaluated derivative du/dx
+    return 0;
+}
